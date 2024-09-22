ad50b251bf43a39d7ad72917851a8eccbb282df9
tuanflute275 <tuanflute275@gmail.com>
2024-09-22 20:44:02 +0700

    build project default v1

---
+++ ShopApp/Areas/Admin/Controllers/HomeController.cs
@@ -0,0 +1,28 @@
+﻿using AspNetCoreHero.ToastNotification.Abstractions;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using NToastNotify;
+using ShopApp.Data;
+
+namespace ShopApp.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [Authorize]
+    [Route("Admin")]
+    public class HomeController : Controller
+    {
+        private readonly ShopAppAspWebContext _context;
+        private readonly INotyfService _toastNotification;
+        public HomeController(ShopAppAspWebContext context, INotyfService toastNotification)
+        {
+            _context = context;
+            _toastNotification = toastNotification;
+        }
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
